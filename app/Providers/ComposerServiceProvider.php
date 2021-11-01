7fe0534fe9f9c55de2d1e436e1d3a1f4325bd17f
Tamracar_9898sreya <tamrakar.shreyaa@gmail.com>
2021-11-01 21:53:46 +0545

    blockfood section dynamic

--- app/Providers/ComposerServiceProvider.php
+++ app/Providers/ComposerServiceProvider.php
@@ -3,6 +3,7 @@
 
 namespace App\Providers;
 
+use App\Http\ViewComposers\Front\HomePageViewComposer;
 use App\Http\ViewComposers\Front\LogoPageViewComposer;
 use App\Http\ViewComposers\Front\MenuPageViewComposer;
 use Carbon\Laravel\ServiceProvider;
@@ -15,6 +16,7 @@ class ComposerServiceProvider extends ServiceProvider
        
 
         View::composer('frontend/layout/menu',MenuPageViewComposer::class);
+        View::composer('frontend/home',HomePageViewComposer::class);
         View::composer('frontend/layout/logo',LogoPageViewComposer::class);
       
     }
