e7800cb510d396b50f3aa811808d59d9569af61f
Tamracar_9898sreya <tamrakar.shreyaa@gmail.com>
2021-11-30 17:11:47 +0545

    few chnages in homecontroller

--- app/Http/Controllers/FrontEnd/HomeController.php
+++ app/Http/Controllers/FrontEnd/HomeController.php
@@ -41,9 +41,9 @@ class HomeController extends BaseController
 
     public function aboutUs()
     {
-        return view('frontend.content.aboutUs');
+        return view('frontend.content.aboutus');
     }
     public function contactUs(){
-        return view('frontend.content.contactUs');
+        return view('frontend.content.contactus');
     }
 }
